3e40cd6a7917e09a85e409b1983a3d490e27410b
Kevin Le <kevinle108@gmail.com>
2023-02-12 13:48:43 -0500

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="mx-4 px-4 text-3xl font-bold text-red-500 underline">Hello world!</h1>
